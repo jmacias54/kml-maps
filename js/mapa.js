b0ce243d001fe271421fa13f5eeb80b73e161dea
Hitss <jesusmaciasbenitez@gmail.com>
2019-03-05 12:59:09 -0600

    first commit

---
+++ js/mapa.js
@@ -0,0 +1,45 @@
+
+       /*
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: {lat: 8.376803, lng: -82.3504809}
+        });
+
+       
+        var ctaLayer = new google.maps.KmlLayer({
+          url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
+          map: map
+        });
+        
+
+        var kmzLayer = new google.maps.KmlLayer('https://cladmin-dev.tmx-internacional.net/portal/pa/cobertura/doc.kml');
+        kmzLayer.setMap(map);
+      }
+*/
+
+
+      var map;
+      var src = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
+      var src_panama = 'https://cladmin-dev.tmx-internacional.net/portal/pa/cobertura/test.kml';
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: new google.maps.LatLng(9.020728,-79.464694),
+          zoom: 11,
+          mapTypeId: 'terrain'
+        });
+
+        var kmlLayer = new google.maps.KmlLayer(src_panama, {
+          suppressInfoWindows: true,
+          preserveViewport: false,
+          map: map
+        });
+        kmlLayer.addListener('click', function(event) {
+          var content = event.featureData.infoWindowHtml;
+          var testimonial = document.getElementById('capture');
+          testimonial.innerHTML = content;
+        });
+      }
